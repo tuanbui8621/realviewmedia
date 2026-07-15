25b5518f8807b7c511bd2aa7cf25df875f1a6d32
tuanbui8621 <tuanbui8621@gmail.com>
2026-07-15 10:54:00 +0700

    cloudflare capability

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,7 @@ import type { NextConfig } from "next";
 const withNextIntl = createNextIntlPlugin('./i18n.ts');
 
 const nextConfig: NextConfig = {
+  output: 'export',
   images: {
     unoptimized: true,
   },
