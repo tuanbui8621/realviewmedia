c7d8bf355bd2968663d0745d6abddc1bb70d5feb
tuanbui8621 <tuanbui8621@gmail.com>
2026-07-31 14:36:56 +0700

    Make favicon background circular

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -32,7 +32,7 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       {
-        url: "/images/favicon.png?v=5",
+        url: "/images/favicon.png?v=6",
         type: "image/png",
         sizes: "192x192",
       },
